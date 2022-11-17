2eabd190e3134ca5d5175b1a6e0e4e5328bb5d69
Gonzalo Acosta <gonzaloacostapeiro@gmail.com>
2022-11-17 18:29:33 -0300

    bump

---
+++ Makefile
@@ -0,0 +1,8 @@
+terraform/init:
+	@cd terraform && terraform init
+
+terraform/plan:
+	@cd terraform && terraform plan
+
+terraform/apply:
+	@cd terraform && terraform apply
---
+++ todos/delete.py
@@ -0,0 +1,21 @@
+import os
+
+import boto3
+dynamodb = boto3.resource('dynamodb')
+
+def delete(event, context):
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+
+    # delete the todo from the database
+    table.delete_item(
+        Key={
+            'id': event['pathParameters']['id']
+        }
+    )
+
+    # create response
+    response = {
+        "statusCode": 200
+    }
+
+    return response
